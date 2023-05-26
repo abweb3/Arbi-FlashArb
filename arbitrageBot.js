7ab5436f9dce8429d64b3e6fdb7a67dbd92d18f8
alkarim07 <alkarim@moralis.io>
2023-05-25 22:03:02 -0300

    ArbBot-Upgrades

--- arbitrageBot.js
+++ arbitrageBot.js
@@ -28,6 +28,13 @@ const gasLimit = 500000; // Adjust the gas limit as desired
 // Slippage tolerance settings
 const slippageTolerance = 0.005; // 0.5% slippage tolerance, adjust as desired
 
+// Configuration settings
+const config = {
+  gasPrice,
+  gasLimit,
+  slippageTolerance,
+};
+
 async function analyzeAndArbitrage() {
   try {
     // Fetch reserves and prices from Uniswap, SushiSwap, and PancakeSwap
@@ -51,6 +58,7 @@ async function analyzeAndArbitrage() {
       await executeFlashLoan(uniswapAmount, poolAddressesProviderAddress, tokenAddress);
 
       // ... perform Uniswap trade with uniswapAmount ...
+      await performUniswapTrade(uniswapAmount, config);
     } else if (sushiswapPrice > uniswapPrice && sushiswapPrice > pancakeSwapPrice) {
       const profit = calculateProfit(sushiswapPrice, uniswapPrice, pancakeSwapPrice);
       const { sushiswapAmount, uniswapAmount } = calculateTradeAmounts(profit, sushiswapPrice, uniswapPrice);
@@ -62,6 +70,7 @@ async function analyzeAndArbitrage() {
       await executeFlashLoan(sushiswapAmount, poolAddressesProviderAddress, tokenAddress);
 
       // ... perform SushiSwap trade with sushiswapAmount ...
+      await performSushiSwapTrade(sushiswapAmount, config);
     } else if (pancakeSwapPrice > uniswapPrice && pancakeSwapPrice > sushiswapPrice) {
       const profit = calculateProfit(pancakeSwapPrice, uniswapPrice, sushiswapPrice);
       const { pancakeSwapAmount, uniswapAmount } = calculateTradeAmounts(profit, pancakeSwapPrice, uniswapPrice);
@@ -73,6 +82,7 @@ async function analyzeAndArbitrage() {
       await executeFlashLoan(pancakeSwapAmount, poolAddressesProviderAddress, tokenAddress);
 
       // ... perform PancakeSwap trade with pancakeSwapAmount ...
+      await performPancakeSwapTrade(pancakeSwapAmount, config);
     } else {
       console.log('No arbitrage opportunity detected.');
     }
@@ -97,8 +107,8 @@ function calculateProfit(sourcePrice, targetPrice1, targetPrice2) {
   const amount = ethers.utils.parseEther('1'); // Amount to be borrowed in the flash loan
   const fee = amount.mul(5).div(10000); // Flash loan fee of 0.05%
 
-  const targetAmount1 = amount.div(sourcePrice).mul(1 - slippageTolerance);
-  const targetAmount2 = targetAmount1.div(targetPrice1).mul(1 - slippageTolerance);
+  const targetAmount1 = amount.div(sourcePrice).mul(1 - config.slippageTolerance);
+  const targetAmount2 = targetAmount1.div(targetPrice1).mul(1 - config.slippageTolerance);
 
   const profit = targetAmount2.mul(targetPrice2).sub(amount.sub(fee));
 
@@ -113,8 +123,8 @@ async function executeFlashLoan(amount, poolAddressesProviderAddress, tokenAddre
   );
 
   const overrides = {
-    gasLimit: gasLimit,
-    gasPrice: gasPrice,
+    gasLimit: config.gasLimit,
+    gasPrice: config.gasPrice,
   };
 
   const transaction = await flashLoanReceiver.executeOperation(
@@ -122,20 +132,120 @@ async function executeFlashLoan(amount, poolAddressesProviderAddress, tokenAddre
     [amount], // amounts
     [0], // premiums (set to 0 for simplicity, adjust if needed)
     signer.address, // initiator
-    '0x' // empty params (replace with actual params if needed)
+    '0x', // empty params (replace with actual params if needed)
+    overrides
   );
 
   console.log('Flash loan transaction hash:', transaction.hash);
 }
 
 function calculateTradeAmounts(amount, sourcePrice, targetPrice) {
-  const targetAmount = amount.div(sourcePrice).mul(1 - slippageTolerance);
-  const sourceAmount = targetAmount.div(targetPrice).mul(1 - slippageTolerance);
+  const targetAmount = amount.div(sourcePrice).mul(1 - config.slippageTolerance);
+  const sourceAmount = targetAmount.div(targetPrice).mul(1 - config.slippageTolerance);
 
   return {
     uniswapAmount: sourceAmount,
     sushiswapAmount: targetAmount,
   };
+}
+
+async function performUniswapTrade(amount, config) {
+  try {
+    // Perform the Uniswap trade using the provided amount and config
+    // Replace the code below with the actual trading logic for Uniswap
+
+    // Connect to the Uniswap contract using ethers.js
+    const uniswapContract = new ethers.Contract(config.uniswapContractAddress, UniswapABI, signer);
+
+    // Create the trade transaction
+    const tradeTx = await uniswapContract.trade(
+      config.tokenIn,
+      config.amountIn,
+      config.tokenOut,
+      config.minAmountOut,
+      config.recipient,
+      config.deadline,
+      config.gasPrice,
+      config.gasLimit,
+      config.value
+    );
+
+    console.log('Uniswap trade transaction hash:', tradeTx.hash);
+
+    // Wait for the transaction to be mined
+    await tradeTx.wait();
+
+    console.log('Uniswap trade completed successfully!');
+  } catch (error) {
+    console.error('Error executing Uniswap trade:', error);
+    // Implement error handling logic here
+  }
+}
+
+async function performSushiSwapTrade(amount, config) {
+  try {
+    // Perform the SushiSwap trade using the provided amount and config
+    // Replace the code below with the actual trading logic for SushiSwap
+
+    // Connect to the SushiSwap contract using ethers.js
+    const sushiSwapContract = new ethers.Contract(config.sushiSwapContractAddress, SushiSwapABI, signer);
+
+    // Create the trade transaction
+    const tradeTx = await sushiSwapContract.trade(
+      config.tokenIn,
+      config.amountIn,
+      config.tokenOut,
+      config.minAmountOut,
+      config.recipient,
+      config.deadline,
+      config.gasPrice,
+      config.gasLimit,
+      config.value
+    );
+
+    console.log('SushiSwap trade transaction hash:', tradeTx.hash);
+
+    // Wait for the transaction to be mined
+    await tradeTx.wait();
+
+    console.log('SushiSwap trade completed successfully!');
+  } catch (error) {
+    console.error('Error executing SushiSwap trade:', error);
+    // Implement error handling logic here
+  }
+}
+
+async function performPancakeSwapTrade(amount, config) {
+  try {
+    // Perform the PancakeSwap trade using the provided amount and config
+    // Replace the code below with the actual trading logic for PancakeSwap
+
+    // Connect to the PancakeSwap contract using ethers.js
+    const pancakeSwapContract = new ethers.Contract(config.pancakeSwapContractAddress, PancakeSwapABI, signer);
+
+    // Create the trade transaction
+    const tradeTx = await pancakeSwapContract.trade(
+      config.tokenIn,
+      config.amountIn,
+      config.tokenOut,
+      config.minAmountOut,
+      config.recipient,
+      config.deadline,
+      config.gasPrice,
+      config.gasLimit,
+      config.value
+    );
+
+    console.log('PancakeSwap trade transaction hash:', tradeTx.hash);
+
+    // Wait for the transaction to be mined
+    await tradeTx.wait();
+
+    console.log('PancakeSwap trade completed successfully!');
+  } catch (error) {
+    console.error('Error executing PancakeSwap trade:', error);
+    // Implement error handling logic here
+  }
 }
 
 // Execute the bot at a specified interval
